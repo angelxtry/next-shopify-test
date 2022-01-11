31ad74752a872c8145eee6fca5979286515b4ebf
sean <sean@gomicorp.com>
2022-01-11 19:59:57 +0900

    global, Layout 스타일 추가

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,7 +1,13 @@
 import { FC } from "react";
 
+import s from "./Layout.module.css";
+
 const Layout: FC = ({ children }) => {
-  return <div className={"layout"}>{children}</div>;
+  return (
+    <div className={"fit"}>
+      <main className={s.root}>{children}</main>;
+    </div>
+  );
 };
 
 export default Layout;
